0aaa35d1e5d25835e97ad2298654bbfcccd78a5b
Samanta00 <80990432+Samanta00@users.noreply.github.com>
2022-07-14 20:25:50 -0300

    Add files via upload

---
+++ projeto_teste/Javascript/js_frontand/cadastro.js
@@ -0,0 +1,3 @@
+alert('Caso o comando de enviar o CPF de errado atualize a página e preencha ele como primeiro campo, pois a página guarda muitos caches')
+alert('Caso o cpf continue errado ele não se enquadra na conta matemática por trás do código')
+alert('Caso teu endereço continue errado mesmo enviando certo é problema de cache pois o código liga direto ao endereço de uma api')
---
+++ projeto_teste/Javascript/js_frontand/index.js
@@ -0,0 +1,3 @@
+alert("Essa página foi desenvolvida para ilustrar o quão igual a desenvolvedora 'Ellen Samanta' consegue programar uma página de acordo com o rascunho. OBS:a página tem algumas modificações pois não foi possivel encontrar todos os layout na internet, porém não foge dos padrões. " )
+
+alert('Espero Que goste, Bem vindo(a)')
